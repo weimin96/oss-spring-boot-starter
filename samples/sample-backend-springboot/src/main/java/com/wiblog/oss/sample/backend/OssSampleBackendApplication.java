078993e6749c2ceb06e3bac941644e3cfe2cf398
panweimin <panweimin@gdcattsoft.com>
2026-04-06 21:54:21 +0800

    feat:优化目录

---
+++ samples/sample-backend-springboot/src/main/java/com/wiblog/oss/sample/backend/OssSampleBackendApplication.java
@@ -0,0 +1,15 @@
+package com.wiblog.oss.sample.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pwm
+ */
+@SpringBootApplication
+public class OssSampleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssSampleBackendApplication.class, args);
+    }
+}
